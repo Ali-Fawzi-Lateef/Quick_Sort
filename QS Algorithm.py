12224fb351cfeb3951b471fb18a0b71de2db6275
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-09-26 21:16:13 +0300

    Update QS Algorithm.py

--- QS Algorithm.py
+++ QS Algorithm.py
@@ -7,7 +7,7 @@ def quicksort(array): # declaring a function named quicksort.
     pivot = array[0]
     # sub-array of all the elements less than the pivot.
     less = [i for i in array[1:] if i <= pivot]
-    # sub-array of all the elements greater than the pivot
+    # sub-array of all the elements greater than the pivot.
     greater = [i for i in array[1:] if i > pivot]
     return quicksort(less) + [pivot] + quicksort(greater)
 
